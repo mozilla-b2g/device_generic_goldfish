09abdf02cd2a0a471e48ceb0dfd5bf49e121df5d
Jonathan Hao <jhao@mozilla.com>
2015-03-25 14:19:24 +0800

    Move all files from /system/media/streaming to /data/misc/streaming/media/

--- darwinstreamingserver/init.goldfish.dss.sh
+++ darwinstreamingserver/init.goldfish.dss.sh
@@ -3,8 +3,9 @@
 src_config_fname=/system/etc/streaming/streamingserver.xml
 dst_config_fname=/data/misc/streaming/streamingserver.xml
 
-src_media_fname=/system/media/streaming/sample_h264_100kbit.mp4
-dst_media_fname=/data/misc/streaming/media/sample_h264_100kbit.mp4
 
 cat $src_config_fname > $dst_config_fname
-cat $src_media_fname > $dst_media_fname
+
+for media_fname in $(ls /system/media/streaming); do
+	cat /system/media/streaming/$media_fname > /data/misc/streaming/media/$media_fname
+done
